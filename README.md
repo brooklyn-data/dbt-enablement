458dc4e09e68943a712736cf1ab334ea001b2bf0
Meredith Alder <102993772+Mer-Alder@users.noreply.github.com>
2022-06-09 11:34:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Brooklyn Data Co. - dbt enablement
+
+## Overview
+This ecommerce shell project is a part of the Brooklyn Data Co. (BDC) dbt enablement program, which is focused on CLI (command line interface) development.
+
+## Related resources
+- The BDC dbt enablement data set (provided by a BDC instructor prior to session 1)
+- A data warehouse to host the training data and faciliate data exploration
---
+++ analysis/first_order_customers.sql
@@ -0,0 +1,21 @@
+with 
+
+orders_by_customer as (
+    select
+        customer_id
+        , min(order_date) as first_order_date
+        , count(*) as order_count
+    from data_loader.website.orders
+    where order_status <> 'Canceled'
+    group by customer_id
+)
+
+select
+    orders.first_order_date
+    , datediff(day, orders.first_order_date, getdate()) as days_since_order
+    , customers.id as customer_id
+    , customers.email
+    , customers.first || ' ' || customers.last as customer_name
+from data_loader.crm.basecustomer__c as customers
+join orders_by_customer as orders on customers.id = orders.customer_id
+where orders.order_count = 1
